4baa1ffd2731047ca0c46645bda3376703b7a2e7
ArmanVD <Arman22.av@gmail.com>
2025-04-04 14:58:20 +0200

    (u)ssr

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 import express from "express";
+import { Liquid } from "liquidjs";
 
 const app = express();
 
 app.use(express.static("public"));
 
+const engine = new Liquid();
+
+app.engine("liquid", engine.express());
+
+app.set("views", "./views");
+
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async function (request, response) {
+  response.render("home.liquid");
+});
+
+app.get("/wiki", async function (request, response) {
+  response.render("wiki.liquid");
+});
 
 app.set("port", process.env.PORT || 8000);
 
